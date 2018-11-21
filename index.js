2bf236379db3cc4e3162ee7bba732b81a584b434
skillarchiedavis <43871788+skillarchiedavis@users.noreply.github.com>
2018-11-20 23:49:38 -0500

    final

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 window.onload = main;
 
-function ajax_word_query(word){
+function ajax_word_query(word, all){
     let httpRequest = new XMLHttpRequest();
-    let url = `/request.php?q=${word}`;
+    let url = `/request.php?q=${word}&all=${all}`;
     
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState === XMLHttpRequest.DONE ){
             if(httpRequest.status === 200){
-                  document.getElementById("update").innerHTML = httpRequest.responseText;
+                if(all){
+                   parse_xml_result(httpRequest.responseXML); 
+                }else{
+                    document.getElementById("update").innerHTML = httpRequest.responseText;
+                }
             }else{
                 alert("There was some error");
             }
@@ -18,18 +22,34 @@ function ajax_word_query(word){
     httpRequest.send();
 }
 
-function update_word_results(response){
-    document.getElementById("update").innerHTML = response;
+function parse_xml_result(response){
+    let definitions = response.documentElement.children;
+    let results = "<ol>";
+    
+    for(var i = 0; i < definitions.length; i++){
+        results+=`<li><h3>${definitions[i].attributes[0].value.toUpperCase()}</h3><p>${definitions[i].innerHTML.trim()}</p><p> - ${definitions[i].attributes[1].value}</p></li>`;
+    }
+    
+    results+= "</ol>"
+     document.getElementById("update").innerHTML =results;
 }
+
+
 
 function main(){
     let search = $("#search")[0];
+    let searchAll = $("#all")[0];
+    
     search.onclick = function(event){
         event.preventDefault();
         ajax_word_query(document.getElementsByName("q")[0].value.trim().toLowerCase(),false);
         document.getElementsByName("q")[0].value = "";
     };
     
-  
+    searchAll.onclick = function(event){
+        event.preventDefault();
+        ajax_word_query('', true);
+        document.getElementsByName("q")[0].value = "";
+    }
     
 }
